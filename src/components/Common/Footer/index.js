784404871df56fb73273d23f9671742bfb21f2d6
cok2exe <cok2exe@gmail.com>
2019-04-21 16:35:27 +0900

    chore

--- src/components/Common/Footer/index.js
+++ src/components/Common/Footer/index.js
@@ -8,16 +8,6 @@ export default class FooterComponent extends PureComponent {
     return (
       <div className="footer">
         <div className="container">
-          <div className="footer__info">
-            Developed by. luda{' '}
-            <Link to="https://github.com/kkangil" target="_blank">
-              @kkangil
-            </Link>
-            <span>&</span>
-            <Link to="https://github.com/cok2exe" target="_blank">
-              @cok2exe
-            </Link>
-          </div>
           <ul className="footer__reference">
             <li>
               icons by. <Link to="https://icons8.com/">icons8.com</Link>
